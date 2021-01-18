a7059f21204943bcc06dc8426bfd05e8d51269d7
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-01-19 02:13:23 +0300

    Init repo

---
+++ app/src/main/java/com/example/jetcaster/ui/JetcasterApp.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.jetcaster.ui
+
+import android.content.Context
+import android.net.ConnectivityManager
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.AmbientContext
+import androidx.compose.ui.res.stringResource
+import com.example.jetcaster.R
+import com.example.jetcaster.ui.home.Home
+
+@Composable
+fun JetcasterApp() {
+    val context = AmbientContext.current
+    var isOnline by remember { mutableStateOf(checkIfOnline(context)) }
+
+    // TODO: add some navigation
+    if (isOnline) {
+        Home()
+    } else {
+        OfflineDialog { isOnline = checkIfOnline(context) }
+    }
+}
+
+// TODO: Use a better way to check internet connection
+@Suppress("DEPRECATION")
+private fun checkIfOnline(context: Context): Boolean {
+    val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetwork = cm.activeNetworkInfo
+    return activeNetwork?.isConnectedOrConnecting == true
+}
+
+@Composable
+fun OfflineDialog(onRetry: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = {},
+        title = { Text(text = stringResource(R.string.connection_error_title)) },
+        text = { Text(text = stringResource(R.string.connection_error_message)) },
+        confirmButton = {
+            TextButton(onClick = onRetry) {
+                Text(stringResource(R.string.retry_label))
+            }
+        }
+    )
+}
